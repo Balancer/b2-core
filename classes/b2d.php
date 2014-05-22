bf04660e982c637628a21527f5a9e1944da754f9
Balancer <balancer@balancer.ru>
2014-05-22 21:31:08 +0400

    Экспериментируем с отладкой.

--- classes/b2d.php
+++ classes/b2d.php
@@ -1,11 +1,19 @@
 <?php
 
-
+// http://stackoverflow.com/questions/6079492/php-how-to-print-a-debug-log
 
 class b2d // bors2-debug
 {
-	function d($var)
+	static function d($var)
 	{
-		Kint::dump($var);
+//		ob_start();
+//		// https://github.com/raveren/kint
+//		// http://raveren.github.io/kint/#advanced
+//		Kint::dump($var);
+//		$s = ob_get_contents();
+//		ob_end_clean();
+		// http://phpdebugbar.com/docs/rendering.html
+		// https://github.com/maximebf/php-debugbar
+		$GLOBALS['debugbar']['messages']->addMessage(var_export($var, true), 'debug', false);
 	}
 }
